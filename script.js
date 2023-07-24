a12ceb74df86130b9d0135c3842a6fe35d53f2da
omgwenxx <e1307685@student.tuwien.ac.at>
2023-07-24 15:46:24 +0200

    Change to play through browser

--- script.js
+++ script.js
@@ -1,64 +1,71 @@
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+let playerScore = 0;
+let computerScore = 0;
+const maxScore = 5;
+
+function playRound(playerSelection) {
+  // Generate computer's choice (rock, paper, or scissors)
+  const choices = ['rock', 'paper', 'scissors'];
+  const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+
+  // Determine the winner
+  let roundResult;
+  if (playerSelection === computerSelection) {
+    roundResult = "It's a tie!";
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    playerScore++;
+    roundResult = `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    computerScore++;
+    roundResult = `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+
+  // Update the displayed results and score
+  const resultsDiv = document.getElementById('results');
+  resultsDiv.textContent = roundResult;
+  
+  const scoreDiv = document.getElementById('score');
+  scoreDiv.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+
+  // Check for a winner
+  if (playerScore === maxScore) {
+    announceWinner("You");
+  } else if (computerScore === maxScore) {
+    announceWinner("Computer");
+  }
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+function announceWinner(winner) {
+  const winnerDiv = document.createElement('div');
+  winnerDiv.textContent = `${winner} are the winner of the game!`;
+  winnerDiv.style.fontWeight = 'bold';
+  winnerDiv.style.fontSize = '18px';
+  winnerDiv.style.marginTop = '20px';
+  document.body.appendChild(winnerDiv);
 
-    const validChoices = ["rock", "paper", "scissors"];
-    if (!validChoices.includes(playerSelection)) {
-        return "Invalid choice. Please enter 'rock', 'paper', or 'scissors'.";
-    }
-
-    if (playerSelection === computerSelection) {
-        return "tie";
-    } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        return "win";
-    } else {
-        return "lose";
-    }
+  // Disable buttons after a winner is announced
+  const buttons = document.getElementsByTagName('button');
+  for (const button of buttons) {
+    button.disabled = true;
+  }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+// Add event listeners to the buttons
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
 
-    for (let round = 1; round <= 5; round++) {
-        const playerChoice = prompt(`Round ${round}: Enter your choice (rock, paper, or scissors):`).toLowerCase();
-        const computerChoice = getComputerChoice();
+rockButton.addEventListener('click', function () {
+  playRound('rock');
+});
 
-        console.log(`Round ${round} - Computer chose: ${computerChoice}`);
+paperButton.addEventListener('click', function () {
+  playRound('paper');
+});
 
-        const result = playRound(playerChoice, computerChoice);
-
-        if (result === "win") {
-            playerScore++;
-            console.log(`You Win! ${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`);
-        } else if (result === "lose") {
-            computerScore++;
-            console.log(`You Lose! ${computerChoice.toUpperCase()} beats ${playerChoice.toUpperCase()}`);
-        } else {
-            console.log("It's a tie!");
-        }
-    }
-
-    console.log("Game Over!");
-    console.log(`Final Score - You: ${playerScore} - Computer: ${computerScore}`);
-
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You are the winner!");
-    } else if (playerScore < computerScore) {
-        console.log("Sorry, you lost the game. Better luck next time!");
-    } else {
-        console.log("The game is a tie!");
-    }
-}
-
-// Start the game
-game();
+scissorsButton.addEventListener('click', function () {
+  playRound('scissors');
+});
